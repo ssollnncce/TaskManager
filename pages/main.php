4770907d5e005e3f7dfb376378d70c0990e12df8
ssollnncce <bgumerov05@gmail.com>
2024-04-11 19:25:40 +0500

    update main page

--- pages/main.php
+++ pages/main.php
@@ -4,11 +4,23 @@
     <script src="https://kit.fontawesome.com/dfe8606c88.js" crossorigin="anonymous"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/fonts.css">
+    <link rel="stylesheet" href="../style/bootstrap.min.css">
+    <link rel="stylesheet" href="../style/colors.css">
     <link rel="stylesheet" href="../style/main-style.css">
+    <link rel="stylesheet" href="../style/fonts.css">
     <title>TaskManager</title>
 </head>
 <body>
-    HELLO
+    <div class="nav-panel">
+        <div class="header">
+            <div class="logo"></div>
+                <h4>TaskMinder.</h4>
+        </div>
+        <nav>
+            <ul></ul>
+        </nav>
+        <div class="logout"></div>
+    </div>
+    <div class="main-panel"></div>
 </body>
 </html>
